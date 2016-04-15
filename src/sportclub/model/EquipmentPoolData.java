ac45cc8d2a15d16367ba5a4d79fc02f1602cb420
paulgr2008 <paulgr2008@gmail.com>
2016-04-15 14:02:39 +0300

    q

---
+++ src/sportclub/model/EquipmentPoolData.java
@@ -0,0 +1,48 @@
+package sportclub.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name ="equipmentPoolData")
+public class EquipmentPoolData {
+		
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private boolean deleted;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="equipmentPoolId")
+	private EquipmentPool equipmentPool;
+	
+	private int quantity;
+
+	public EquipmentPoolData() {
+		super();
+	}
+
+	public EquipmentPool getEquipmentPool() {
+		return equipmentPool;
+	}
+
+	public void setEquipmentPool(EquipmentPool equipmentPool) {
+		this.equipmentPool = equipmentPool;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+}
